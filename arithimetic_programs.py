ff66e813df88c9f2dac322f6b8c1a356e61e0476
AshwinLeo11 <ashwinbalakrishnan11@gmail.com>
2023-08-25 21:30:50 +0530

    ifelse added

--- arithimetic_programs.py
+++ arithimetic_programs.py
@@ -15,3 +15,23 @@ if __name__ == '__main__':
     b = int(input())
     print(a//b)
     print(a/b)
+
+
+# if else excercise 3 :
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2!=0:
+        print("Weird")
+    elif n%2==0:
+        if n in range (2,6):
+            print("Not Weird")
+        elif n in range (6,21):
+            print("Weird")
+        else:
+            print("Not Weird")
+            
+
+
+
+
