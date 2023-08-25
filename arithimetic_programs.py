ec94ef467bd9435ee2d0cb953b3323e16355cabb
AshwinLeo11 <ashwinbalakrishnan11@gmail.com>
2023-08-25 20:54:47 +0530

    Arithmetic programs solved

---
+++ arithimetic_programs.py
@@ -0,0 +1,17 @@
+# excercise 1 :
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
+# excercise 2 :
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
